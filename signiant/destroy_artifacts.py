df6ae1a476d9edc4a5765c60ad2cc4d5549fbab3
Matthew Corner <mcorner@signiant.com>
2015-10-20 17:57:13 -0400

    da: Added duplicate detection

--- signiant/destroy_artifacts.py
+++ signiant/destroy_artifacts.py
@@ -1,3 +1,9 @@
+"""
+destroy_artifacts.py
+
+Goes through current jobs in Jenkins and finds artifacts that have been deleted in Jenkins, but not in the artifact share. Sort of flexible, but it ties into Signiant's current build patterns fairly tightly in some areas.
+"""
+
 import sys,os
 
 from argparse import ArgumentParser
@@ -5,7 +11,8 @@ from ConfigParser import RawConfigParser
 from maestro.jenkins.jobs import EnvironmentVariableJobEntry, InvalidEntryError
 from maestro.tools import string, path
 
-# Globals used throughout the script
+## Globals used throughout the script
+
 VERBOSE = False
 DEBUG = False
 
@@ -22,25 +29,45 @@ CONFIG_PATH = os.path.join("./config")
 ENVIRONMENT_VARIABLES = []
 
 #CONFIG: Environment Variables potentially containing deployment paths (i.e. paths to search for builds)
-#The script will attempt to replace environment variables in the paths
+#The script will attempt to replace $VARIABLES with their corresponding value from ENVIRONMENT_VARIABLES
 DEPLOYMENT_PATHS = []
 
-#Jobs to ignore
+#CONFIG: REGEX to apply to the subfolders of the deployment directorys. (i.e. {[0-9]+} )
+BUILD_FOLDER_REGEX = ""
+
+#CONFIG: Jobs to ignore (use the job name)
 IGNORE_JOBS = []
 
-#Where to strip the deployment paths.
-#Signiant: strip on $PROJECT_FAMILY, and prepend /Releases/Jenkins/$PROJECT_FAMILY
-STRIP_ROOT = {}
+#CONFIG: Where to split the deployment paths, the script takes the second index ( [1] ) 
+#Signiant: Default in config is to split on $PROJECT_FAMILY
+SPLIT_TOKEN = ""
+
+#CONFIG: What to prepend to the string[1] from deployment path split with SPLIT_TOKEN
+#Signiant: Default in config is to 
+PREPEND_STRING = ""
+
+#CONFIG: What to append to the  string[1] frpm deployment path split with SPLIT_TOKEN
+APPEND_STRING = ""
+
+#Tracks dupicates by having $PROJECT_FAMILY:$PROJECT_TITLE:$PROJECT_BRANCH as a key, and the name of the entry as a value (for error messaging)
+__duplicate_tracker__ = dict()
+
+#Tracks duplicate errors
+__duplicates__ = list()
 
 #We pass in undeleted_paths set in order to avoid duplicates, and get an accurate byte clean up count
 #Makes it fairly slow, but whatever. It's still under a minute for scanning the entire thing
 
 def __get_undeleted_artifact_paths__(entry, release_paths, undeleted_paths_set = None):
+    """
+    Loop through release paths and see if we can find anything with Build-XXX, strip the number out and compare to the job entry. Put all ones not in the job entry into a set.
+    """
     if not isinstance(entry, EnvironmentVariableJobEntry) or entry is None:
         raise TypeError("You must pass in a EnvironmentVariableJobEntry!")
     if undeleted_paths_set is None:
         undeleted_paths_set = set()
     for path in release_paths:
+        #TODO: Find a better way to do this
         #We need to strip off any deploy path that has Build-$BUILD_NUMBER at the end
         if path.endswith("$BUILD_NUMBER"):
             #This is kind of neat. Prepend a slash, and unpack the list returned from path.split without the last element and join it
@@ -48,6 +75,7 @@ def __get_undeleted_artifact_paths__(entry, release_paths, undeleted_paths_set =
         try:
             for subdir in os.listdir(path):
                 try:
+                    #TODO: Find a better way to do this (dont rely on Build-XXX)
                     build_no = subdir.split("-")[1]
                     if build_no not in entry.get_build_number_list():
                         undeleted_paths_set.add(os.path.join(path,subdir))
@@ -60,24 +88,33 @@ def __get_undeleted_artifact_paths__(entry, release_paths, undeleted_paths_set =
     return undeleted_paths_set
 
 def __enumerate_remote_artifact_config_entries__(jobs_path):
+    """
+    Loop through the found config.xml files and return their folder path
+    """
     for root, dirnames, filenames in os.walk(jobs_path):
         if "config.xml" in filenames:
             try:
-                yield EnvironmentVariableJobEntry(os.path.join(root,"config.xml"))
+                yield EnvironmentVariableJobEntry(root)
             except InvalidEntryError as e:
                 if VERBOSE == 1:
-                    print "Skipping over " + str(os.path.join(root,"config.xml"))
+                    print "Skipping over " + str(root)
 
 def __parse_config__(config_file_path):
+    """
+    Parses the config file and sets the globals.
+    """
     global ENVIRONMENT_VARIABLES
     global DEPLOYMENT_PATHS
     global DEPLOYMENT_STRUCTURES
+    global BUILD_FOLDER_REGEX
     global IGNORE_JOBS
-    global STRIP_ROOT
+    global SPLIT_TOKEN
+    global PREPEND_STRING
+    global APPEND_STRING
 
     config = RawConfigParser()
     config.read(config_file_path)
-    
+    # I wrapped these in trys just in case we want something optional 
     try:
         ENVIRONMENT_VARIABLES = config.get("ArtifactConfig","ENVIRONMENT_VARIABLES").split(',')
     except:
@@ -91,12 +128,23 @@ def __parse_config__(config_file_path):
     except:
         raise
     try:
+        BUILD_FOLDER_REGEX = config.get("ArtifactConfig","BUILD_FOLDER_REGEX")
+    except:
+        raise
+    try:
         IGNORE_JOBS = config.get("ArtifactConfig","IGNORE_JOBS").split(',')
     except:
         raise
     try:
-        keyval = config.get("ArtifactConfig","STRIP_ROOT").split(',')
-        STRIP_ROOT[keyval[0]] = keyval[1]
+        SPLIT_TOKEN = config.get("ArtifactConfig","SPLIT_TOKEN")
+    except:
+        raise
+    try:
+        PREPEND_STRING = config.get("ArtifactConfig","PREPEND_STRING")
+    except:
+        raise
+    try:
+        APPEND_STRING = config.get("ArtifactConfig","APPEND_STRING")
     except:
         raise
 
@@ -107,10 +155,15 @@ def __verify_environment_variables__(entry):
     if not isinstance(entry,EnvironmentVariableJobEntry):
         raise TypeError("Received object of type " + str(type(environment_variable_job)) + " expected type SigniantRemoteArtifactJobEntry.")
     for var in ENVIRONMENT_VARIABLES:
+        if entry.name == "Media Shuttle Store-mjc":
+            print str(entry.environment_variables.keys())
         if var not in entry.environment_variables.keys() or entry.environment_variables[var] is None:
-		raise InvalidEntryError("Required environment variable " + str(var) + " was not found in job entry " + str(entry.name) + ".")
+            raise InvalidEntryError("Required environment variable " + str(var) + " was not found in job entry " + str(entry.name) + ".")
        
 def __get_release_path_list__(entry):
+    """
+    Builds a string replace dictionary out of the environment variables, calls __strip_release_path__, replaces the $VARIABLES with their values (if found), normalizes the path and adds it to a list which is returned by this method.
+    """
     releases = list()
     for key in DEPLOYMENT_PATHS:
         try:
@@ -138,20 +191,26 @@ def __get_release_path_list__(entry):
         return releases 
 
 def __parse_arguments__():
+    """
+    Currently unused and uses defaults defined above
+    """
     pass
 
 def __strip_release_path__(release_path):
+    """
+    Converts UNC/Windows paths into forward slashes, and then splits and pre/appends
+    """
+
     try:
         clean_path = release_path.replace('\\','/')
-        converted_path = os.path.abspath(clean_path)
-        key = STRIP_ROOT.keys()[0]
-        stripped_path = converted_path.split(key)
-        return STRIP_ROOT[key] + "/" + stripped_path[1]
+#        converted_path = os.path.abspath(clean_path)
+        stripped_path = clean_path.split(SPLIT_TOKEN)
+        return PREPEND_STRING + stripped_path[1] + APPEND_STRING
     except:
         return None
     
 
-def destroy_artifacts(is_dry_run):
+def destroy_artifacts():
     if not os.path.exists(CONFIG_PATH):
         raise ValueError("You need to provide a valid config file! Currently looking for: " + str(CONFIG_PATH))
 
@@ -166,34 +225,69 @@ def destroy_artifacts(is_dry_run):
 
     #First we want to go through the config entries that contain Environment Variables from envinject
     for entry in __enumerate_remote_artifact_config_entries__(JENKINS_JOBS_DIRECTORY_PATH):
+        if entry.name == "Media Shuttle Store-mjc":
+            print "Found it!"
         if entry.get_build_number_list() is None:
             continue
         try:
+            if entry.name == "Media Shuttle Store-mjc":
+                print "Bef env"
             __verify_environment_variables__(entry)
+            if entry.name == "Media Shuttle Store-mjc":
+                print "Bef dup"
+            __verify_duplicates__(entry)
+            if entry.name == "Media Shuttle Store-mjc":
+                print "Bef rel"
             release_paths = __get_release_path_list__(entry)
             if release_paths is not None:
                 for undeleted_artifact_path in __get_undeleted_artifact_paths__(entry,release_paths,undeleted_paths_set):
                     pass #Building set...
         #If there's no match to any of the keys, then we don't care about this entry
         except TypeError:
+            if entry.name == "Media Shuttle Store-mjc":
+                raise
             continue
         #If the job doesn't have the variables we're looking for, skip over it
         except InvalidEntryError:
+            if entry.name == "Media Shuttle Store-mjc":
+                raise
             continue
-    
+
+    #Loop through the (now) unique path list so we can get the size and delete 
     for artifact_path in undeleted_paths_set:
         print "Deleting " + str(artifact_path)
         cleaned_byte_count = path.get_tree_size(artifact_path) + cleaned_byte_count
-        if not is_dry_run:
+        if not IS_DRY_RUN:
             pass #Delete things here
 
-    if is_dry_run:
+    if IS_DRY_RUN:
         print "Would have cleaned up " + str(cleaned_byte_count) + " bytes!"
     else:
         print "Cleaned up " + str(cleaned_byte_count) + " bytes!"
 
+    if len(__duplicates__) > 0:
+        print "The job failed because of the following errors:"
+        for duplicate in __duplicates__:
+            print duplicate
+        sys.exit(1)
+
+#TODO: Make less Signiant specific
+def __verify_duplicates__(entry):
+    global __duplicate_tracker__
+    global __duplicates__
+
+    # Key is all environment variables seperated by a colon
+    key = str(entry.environment_variables["PROJECT_FAMILY"] + "/" + entry.environment_variables["PROJECT_TITLE"] + "/" + entry.environment_variables["PROJECT_BRANCH"])
+    #Check for duplicate
+    if key in __duplicate_tracker__.keys():
+        print "DUPLICATE"
+        __duplicates__.append("Attempted to parse entry with name " + str(entry.name) + " but entry with name " + str(__duplicate_tracker__[key]) + " is currently using the same deployment strategy: " + key)
+        raise InvalidEntryError("Found a duplicate entry! Please see error message at the end of the script.")
+    else:
+        __duplicate_tracker__[key] = entry.name
+
 if __name__ == "__main__":
-    destroy_artifacts(IS_DRY_RUN)
+    destroy_artifacts()
 
 
     
